bfb3829e4965ae68dcea17ef8e76255647b7a67f
Vj <meetvijay@live.com>
2015-08-07 08:03:01 -0700

    First katana middleware to show properties of environment in console and in response body

---
+++ VKatana/VKatana.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace VKatana.Console
+{
+    using System;
+    using Microsoft.Owin;
+    using Microsoft.Owin.Hosting;
+    using Owin;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (WebApp.Start<Program>("http://localhost:8080"))
+            {
+                Console.WriteLine("Server started");
+                Console.WriteLine("Press any key to stop the server...");
+                Console.ReadKey();
+                Console.WriteLine("Server stopped!");
+            }
+        }
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseShowEnvironmentInResponseBody();
+            app.UseShowEnvironmentInConsole();
+        }
+    }
+}
---
+++ VKatana/VKatana/VAppBuilderExtensions.cs
@@ -0,0 +1,56 @@
+﻿namespace VKatana
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Owin;
+
+    public static class VAppBuilderExtensions
+    {
+        public static void UseShowEnvironment(
+            this IAppBuilder app,
+            Func<IDictionary<string, object>, string, Task> logAction,
+            string delimiter)
+        {
+            app.Use(
+                async (ctx, next) =>
+                {
+                    IDictionary<string, object> environment = ctx.Environment;
+                    var response = environment["owin.ResponseBody"] as Stream;
+                    var sb = new StringBuilder();
+                    sb.Append($"Total # of keys:{environment.Keys.Count}{delimiter}");
+                    foreach (var key in environment.Keys)
+                    {
+                        sb.Append($"{key}:{environment[key]}{delimiter}");
+                    }
+
+                    await logAction(ctx.Environment, sb.ToString());
+
+                    await next();
+                });
+        }
+
+        public static void UseShowEnvironmentInResponseBody(this IAppBuilder app)
+        {
+            app.UseShowEnvironment(
+                async (environment, message) =>
+                {
+                    var response = environment["owin.ResponseBody"] as Stream;
+                    using (var writer = new StreamWriter(response))
+                    {
+                        await writer.WriteAsync(message);
+                    }
+                },
+                "<br/>");
+        }
+
+        public static void UseShowEnvironmentInConsole(this IAppBuilder app)
+        {
+            app.UseShowEnvironment(
+                async (environment, message) => { await Task.Run(() => Console.WriteLine(message)); },
+                "\n");
+        }
+    }
+}
